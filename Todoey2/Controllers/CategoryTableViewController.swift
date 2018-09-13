e627f7e10553e1bc5559ebf122fb89fb14bd5279
Marius <jaunikis@gmail.com>
2018-09-13 05:49:04 +0100

    pries uzduoti

---
+++ Todoey2/Controllers/CategoryTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CategoryTableViewController.swift
+//  Todoey2
+//
+//  Created by Marius Vorosilovas on 13/09/2018.
+//  Copyright © 2018 Marius Vorosilovas. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    //MARK: - TableView Datasource Methods
+    
+    
+    //MARK: - Data Manipulation Methods
+    
+    
+    //MARK: - Add New Categories
+    
+    
+  
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    
+    
+}
